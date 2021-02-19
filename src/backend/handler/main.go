4dffdea07767ac0185de980dc46f7f7dabc52eff
TomChiu247 <tomchiu2006@gmail.com>
2021-02-19 17:43:02 -0500

    Attempts to fix CORS access issue

--- src/backend/handler/main.go
+++ src/backend/handler/main.go
@@ -11,8 +11,15 @@ type MainResponse struct {
 	Data string `json:"data"`
 }
 
+//func enableCors(w *http.ResponseWriter) {
+//	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+//}
+
 // MainHandler handles main
 func MainHandler(w http.ResponseWriter, r *http.Request) {
+	//w.Header().Set("Content-Type", "text/html; charset=utf-8")
+    w.Header().Set("Access-Control-Allow-Origin", "*")
+
 	data := r.URL.Query().Get("data")
 
 	resp := MainResponse{
